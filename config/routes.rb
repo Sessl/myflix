425a8457762776f4c2bc060b61ad506dc1eb07b8
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-02-22 20:14:10 -0800

    setting 'home(/:action)' to videos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Myflix::Application.routes.draw do
   
-  root to: 'videos#home'
+  #root to: 'videos#home'
 
   get 'ui(/:action)', controller: 'ui'
+  get 'home(/:action)', controller: 'videos'
 
   resources :videos, except: [:destroy]
   resources :categories, except: [:destroy]
