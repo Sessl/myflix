515f3c4c29bedc126bb2d7c7e8b755a3ded12349
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-02-25 13:43:57 -0800

    changed to data retrieval code to use expect

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,6 @@ describe Video do
   it "saves itself" do
     video = Video.new(title: "Lilo and Stitch", description: "Children love it")
     video.save
-    Video.last.title.should == "Lilo and Stitch"
+    expect(Video.last).to eq(video)
   end 
 end
