73cead42c2239640c602748546bbb16e372ab347
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-03-24 14:43:17 -0700

    completing rspec tests for creating reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  before_action :require_user
+
+  def create
+    @video = Video.find(params[:video_id])
+    @review = @video.reviews.build(params.require(:review).permit(:content, :rating).merge!(user: current_user))
+    #@review.user = current_user chaining the method merge instead
+    if @review.save
+      flash[:notice] = "Your review was saved"
+      redirect_to video_path(@video)
+    else
+      @reviews = @video.reviews.reload.latest_reviews
+      render "videos/show"
+    end
+  end  
+
 end
