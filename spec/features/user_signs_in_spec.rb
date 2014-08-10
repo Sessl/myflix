e46f39aab23d2239647a4b4a3ac682f77c024cf1
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-08-10 16:53:39 -0700

    creating sign_in macro for feature specs

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature "Signing in" do
   scenario "Signing in with correct credentials" do
     alice = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", :with => alice.email
-    fill_in "Password", :with => alice.password
-    click_button "Sign in"
+    sign_in(alice)
     page.should have_content alice.username
   end
 end
