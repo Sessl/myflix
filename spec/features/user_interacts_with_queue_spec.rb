465ee037be4ea90cc160c23752b70c9f4cede3a4
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-08-11 13:53:55 -0700

    refactoring feature specs into methods

--- spec/features/user_interacts_with_queue_spec.rb
+++ spec/features/user_interacts_with_queue_spec.rb
@@ -11,42 +11,66 @@ feature "User interacts with the queue" do
   arthur.categories << category
 
   sign_in
-  find("a[href='/videos/#{beckham.id}']").click
-  page.should have_content(beckham.title)
+  go_to_video_page(beckham)
+  expect_page_to_show_title(beckham)        #testing show video page
 
-  click_link "+ My Queue"
-  page.should have_content(beckham.title)
+  click_link_to_enqueue("+ My Queue")
+  expect_page_to_show_title(beckham)
 
   visit video_path(beckham)
-  page.should_not have_content "+ My Queue"
-
-  visit home_path
-  find("a[href='/videos/#{snatch.id}']").click
-  click_link "+ My Queue"
-
-  visit home_path
-  find("a[href='/videos/#{arthur.id}']").click
-  click_link "+ My Queue"
-
-  within(:xpath, "//tr[contains(.,'#{beckham.title}')]") do
-    fill_in "queue_items[][position]", with: 3
-  end
-
-  within(:xpath, "//tr[contains(.,'#{snatch.title}')]") do
-    fill_in "queue_items[][position]", with: 1
-  end
-
-  within(:xpath, "//tr[contains(.,'#{arthur.title}')]") do
-    fill_in "queue_items[][position]", with: 2
-  end
-
-  click_button "Update Instant Queue"
-
-  expect(find(:xpath, "//tr[contains(.,'#{beckham.title}')]//input[@type='text']").value).to eq("3")
-  expect(find(:xpath, "//tr[contains(.,'#{snatch.title}')]//input[@type='text']").value).to eq("1")
-  expect(find(:xpath, "//tr[contains(.,'#{arthur.title}')]//input[@type='text']").value).to eq("2")
+  expect_link_not_to_be_seen("+ My Queue")
   
-  end  
+  add_video_to_queue(snatch)
+  add_video_to_queue(arthur)
 
+  set_video_position(beckham, 3)
+  set_video_position(snatch, 1)
+  set_video_position(arthur, 2)
+
+  update_queue
+
+  expect_video_position(beckham, 3)
+  expect_video_position(snatch, 1)
+  expect_video_position(arthur, 2)
+
+  end 
+  
+
+  def go_to_video_page(video)
+    find("a[href='/videos/#{video.id}']").click
+  end
+
+  def expect_page_to_show_title(video)
+    page.should have_content(video.title)
+  end
+
+  def click_link_to_enqueue(button_text)
+    click_link button_text
+  end
+
+  def expect_link_not_to_be_seen(link_text)
+    page.should_not have_content(link_text)
+
+  end
+
+  def update_queue
+    click_button "Update Instant Queue"
+  end
+
+  def add_video_to_queue(video)
+    visit home_path
+    find("a[href='/videos/#{video.id}']").click
+    click_link "+ My Queue"
+  end
+
+  def set_video_position(video, position)
+    within(:xpath, "//tr[contains(.,'#{video.title}')]") do
+    fill_in "queue_items[][position]", with: position
+  end
+  end
+ 
+  def expect_video_position(video, position)
+    expect(find(:xpath, "//tr[contains(.,'#{video.title}')]//input[@type='text']").value).to eq(position.to_s)
+  end
 
 end
