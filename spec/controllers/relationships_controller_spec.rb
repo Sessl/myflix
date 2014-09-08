3e1ee5da4e89b70de5327e96d39008dfbb164f6a
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2014-09-08 13:55:18 -0700

    passing relationship instead of relationship.id inthe controller spec

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -16,21 +16,33 @@ describe RelationshipsController do
   end
 
   describe "DELETE destroy" do
+    it_behaves_like "requires sign in" do
+      let(:action) {get :index, id: 4}
+    end
     it "deletes relationship with leader" do
       lilo = Fabricate(:user)
       set_current_user(lilo)
       nani = Fabricate(:user)
       relationship = Fabricate(:relationship, leader: nani, follower: lilo)
-      delete :destroy, id: relationship.id
+      delete :destroy, id: relationship
       expect(Relationship.count).to eq(0)
     end
+    it "redirects to people page" do
+      lilo = Fabricate(:user)
+      set_current_user(lilo)
+      nani = Fabricate(:user)
+      relationship = Fabricate(:relationship, leader: nani, follower: lilo)
+      delete :destroy, id: relationship
+      expect(response).to redirect_to people_path
+    end 
+
     it "does not delete a relationship if current_user is not the follower" do
       lilo = Fabricate(:user)
       set_current_user(lilo)
       nani = Fabricate(:user)
       david = Fabricate(:user)
       relationship = Fabricate(:relationship, leader: nani, follower: david)
-      delete :destroy, id: relationship.id
+      delete :destroy, id: relationship
       expect(Relationship.count).to eq(1)
     end
 
