3bfadba9352a5b06c40ecaeb581bb1476caae044
Sessl <sse_p@hotmail.com>
2014-09-30 21:24:31 -0700

    Revert "Week4 part2"

--- app/mailers/myflix_mailer.rb
+++
@@ -1,6 +0,0 @@
-class MyflixMailer < ActionMailer::Base
-  def notify_on_signup(user)
-    @user = user
-    mail from: "info@myflixapp.com", to: user.email, subject: "Welcome to Myflix"
-  end
-end
