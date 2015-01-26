34138e6bde4081951c50989457fb6f46bdb455f3
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-01-26 10:49:46 -0800

    adding password confirmation to password_resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,12 +10,15 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.where(token: params[:token]).first
-    if user 
+    if user && (params[:password] == params[:password_confirmation])
       user.password = params[:password]
       user.token = nil
       user.save
       flash[:success] = "Your password has been changed. Please sign in."
       redirect_to sign_in_path
+    elsif !(params[:password] == params[:password_confirmation])
+      flash[:error] = "Password mismatch"
+      redirect_to password_reset_path(user.token)
     else
       redirect_to expired_token_path
     end
