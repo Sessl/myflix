ea73eca53cd10c0e7cc638fbf6495e254e7f9228
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-11-21 09:44:25 -0800

    refactoring invite a friend feature specs

--- spec/features/user_invites_friend_spec.rb
+++ spec/features/user_invites_friend_spec.rb
@@ -5,38 +5,52 @@ feature 'User invites friend' do
     alice = Fabricate(:user)
     sign_in(alice)
 
+    invite_a_friend
+    friend_accepts_invitation
+    sign_out
+    friend_signs_in
+    friend_should_follow(alice)
+    inviter_should_follow_friend(alice)
 
+    
+
+    clear_email
+  end
+
+  def invite_a_friend
     visit new_invitation_path
     fill_in "Friend's Name", with: "John Doe"
     fill_in "Friend's Email Address", with: "john@example.com"
     fill_in "Message", with: "Hello please join this site."
     click_button "Send Invitation"
     sign_out
-    
+  end
 
+  def friend_accepts_invitation
     open_email "john@example.com"
     current_email.click_link "Accept this invitation"
-
-
     fill_in "Password", with: "password"
     fill_in "Confirm Password", with: "password"
     fill_in "Full Name", with: "John Doe"
     click_button "Sign Up"
+  end
 
-   sign_out
-   click_link "Sign In"
-   fill_in "Email Address", with: "john@example.com"
-   fill_in "Password", with: "password"
-   click_button "Sign in"
+  def friend_signs_in
+    click_link "Sign In"
+    fill_in "Email Address", with: "john@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+  end
 
+  def friend_should_follow(user)
     click_link "People"
-    expect(page).to have_content alice.username
+    expect(page).to have_content user.username
     sign_out
+  end
 
-    sign_in(alice)
+  def inviter_should_follow_friend(inviter)
+    sign_in(inviter)
     click_link "People"
     expect(page).to have_content "John Doe"
-
-    clear_email
   end
 end
