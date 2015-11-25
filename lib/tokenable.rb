3f84c829b6e21151ce5ff5f13e8d8719064f1f39
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-11-25 08:18:47 -0800

    fixing indenting in tokenable and shared_exampls.rb

--- lib/tokenable.rb
+++ lib/tokenable.rb
@@ -2,11 +2,11 @@ module Tokenable
   extend ActiveSupport::Concern
 
   included do
-    before_create :generate_token  	
+    before_create :generate_token   
 
-	  def generate_token
-	    self.token = SecureRandom.urlsafe_base64
-	    self.token_set_time = Time.zone.now
-	  end
-	end
+    def generate_token
+      self.token = SecureRandom.urlsafe_base64
+      self.token_set_time = Time.zone.now
+    end
+  end
 end
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -5,6 +5,7 @@ shared_examples "requires sign in" do
     expect(response).to redirect_to sign_in_path
   end
 end
+
 shared_examples "tokenable" do
     it "generates a random token when the invitation is created" do
     expect(object.token).to be_present
