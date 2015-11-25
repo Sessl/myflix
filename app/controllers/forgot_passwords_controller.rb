79a502cc890ff699e049650dc8d4beb10f3882c5
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-11-25 07:49:42 -0800

    fixing indeting in forgot_passwords_controller again

--- app/controllers/forgot_passwords_controller.rb
+++ app/controllers/forgot_passwords_controller.rb
@@ -3,14 +3,14 @@ class ForgotPasswordsController < ApplicationController
   end
 
   def create
-  	@user = User.where(email: params[:email]).first
-  	if @user
+    @user = User.where(email: params[:email]).first
+    if @user
       @user.generate_password_reset_token
       MyflixMailer.send_forgot_password(@user).deliver
       redirect_to forgot_password_confirmation_path
-  	else
-  	  flash[:error] = params[:email].blank? ? "Email cannot be blank." : "There is no user with that email in the system."
-  	  redirect_to forgot_password_path
+    else
+      flash[:error] = params[:email].blank? ? "Email cannot be blank." : "There is no user with that email in the system."
+      redirect_to forgot_password_path
     end
   end
 
