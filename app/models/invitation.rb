201058f0644f84a530c39fd66a172d31e07cae6f
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-11-25 08:02:16 -0800

    removing commented out code from invitation.rb

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,10 +5,4 @@ class Invitation < ActiveRecord::Base
 
   validates_presence_of :recipient_name, :recipient_email, :message
   
-#  before_create :generate_invitation_token
-
-#  def generate_token
-#  	self.token = SecureRandom.urlsafe_base64
-#    self.token_set_time = Time.zone.now
-# end
 end
