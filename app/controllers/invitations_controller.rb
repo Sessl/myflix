700120d405f138fd95da92be40322480dc08697b
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-11-25 07:52:26 -0800

    fixing indenting in invitations_controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,16 +1,16 @@
 class InvitationsController < ApplicationController
   before_filter :require_user
   def new
-  	@invitation = Invitation.new
+    @invitation = Invitation.new
   end
 
   def create
-  	@invitation = Invitation.new(invitation_params.merge!(inviter_id: current_user.id))
+    @invitation = Invitation.new(invitation_params.merge!(inviter_id: current_user.id))
     
     if @invitation.save
       MyflixMailer.send_invitation_email(@invitation).deliver
       flash[:success] = "You have successfully invited #{@invitation.recipient_name}"
-    	redirect_to new_invitation_path
+      redirect_to new_invitation_path
     else
       flash[:error] = "Please check your inputs"
       render :new
