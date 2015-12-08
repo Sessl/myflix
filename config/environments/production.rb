b2c85b6932b6c6712c958b78ad88bbea04b533be
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2015-12-08 15:39:25 -0800

    adding www to domain in action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Myflix::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
     port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               'sesslmyflix.herokuapp.com',
+    domain:               'www.sesslmyflix.herokuapp.com',
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
