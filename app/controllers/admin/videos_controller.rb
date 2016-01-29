0289d91e96d76c665c5e18a7bf7008c1351a478d
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2016-01-29 09:48:18 -0800

    admin adds new video title, description and categories

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,7 +1,22 @@
 class Admin::VideosController < AdminsController
   def new
+    @video = Video.new
   end
 
   def create
+    @video = Video.new(admin_video_params)
+    if @video.save
+      flash[:success] = "You have successfully added the video '#{@video.title}'."
+      redirect_to new_admin_video_path
+    else
+      flash[:error] = "Your attempt to add a video failed! Please check errors!"
+      render :new
+    end
+  end
+
+  private
+
+  def admin_video_params
+    params.require(:video).permit(:title, :description, category_ids: [])
   end
 end
