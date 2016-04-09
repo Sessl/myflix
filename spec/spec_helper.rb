8e3c2fc9b626181897d6f7dfbf3a024d38b79dfb
Suchitra Edussuriya-Essl <sse_p@hotmail.com>
2016-04-08 21:04:17 -0700

    completing feature tests for stripe payments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,7 +22,7 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
-  ignore_localhost = true
+  c.ignore_localhost = true
 end
 
 RSpec.configure do |config|
